3721f08e3a185b19221030e2073c70561bbd6f20
Jalal Bouri <jalal@hiddenharvestventures.com>
2025-11-17 21:51:53 -0500

    add base request

---
+++ src/spotify/requests/base_request.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+with open("src/spotify/auth/credentials/access_token.json", "r") as f:
+    access_token = json.load(f)["access_token"]
+
+base_url = "https://api.spotify.com/v1"
+
+def base_request(route: str, method: str, params: dict = None, body: dict = None):
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    
+    if body is not None:
+        response = requests.request(method, f"{base_url}{route}", headers=headers, params=params, data=json.dumps(body))
+    else:
+        response = requests.request(method, f"{base_url}{route}", headers=headers, params=params)
+    
+    return response.text
