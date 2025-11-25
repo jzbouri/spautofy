bc66ca80cbd2a8d7c1bf9e0a72ad5257624a6fde
Jalal Bouri <jalal@hiddenharvestventures.com>
2025-11-25 07:30:46 -0500

    add basic cli

--- main.py
+++ main.py
@@ -1,6 +1,70 @@
-def main():
-    print("Hello from spautofy!")
+from src.db.db_manager import DatabaseManager
+import questionary
+import sys
+from rich import print
+from rich.text import Text
+from rich.console import Console
 
+from src.llm.response import handle_user_message
+from src.llm.output import get_output_text, DASH_COUNT
 
-if __name__ == "__main__":
-    main()
+console = Console()
+db_manager = DatabaseManager()
+
+def _print_separator():
+    separator = Text("-" * DASH_COUNT, style="dim")
+    print(separator)
+
+def _clear_previous_line():
+    sys.stdout.write("\033[A\033[2K")
+    sys.stdout.flush()
+
+def _clear_separator_and_input():
+    sys.stdout.write("\033[A\033[2K\033[A\033[2K")
+    sys.stdout.flush()
+
+while True:
+    chats = db_manager.get_all_chats()
+
+    if chats:
+        chat_index = questionary.select(
+            "Select one of the following chats using the arrow keys. Press Enter to confirm your selection.",
+            choices=[f"{i}: {chat['created_at'].strftime('%Y-%m-%d %H:%M:%S')}" for i, chat in enumerate(chats)] + ["Create new chat"]
+        ).ask()
+        
+        if chat_index is None:
+            sys.exit(0)
+            
+        if chat_index == "Create new chat":
+            chat_id = db_manager.store_chat()['id']
+        else:
+            chat_id = chats[int(chat_index.split(":")[0])]['id']
+    else:
+        chat_id = db_manager.store_chat()['id']
+
+    events = [event for event in db_manager.get_chat_events(chat_id)]
+
+    for event in events:
+        print(get_output_text(event['event_object']))
+
+    while True:
+        _print_separator()
+        user_input = input("You (q=quit, s=switch) - ").strip()
+        
+        if not user_input:
+            _clear_previous_line()
+            continue
+
+        if user_input.lower() == 'q':
+            _clear_separator_and_input()
+            sys.exit(0)
+        
+        if user_input.lower() == 's':
+            _clear_separator_and_input()
+            console.clear()
+            break
+
+        _clear_separator_and_input()
+
+        for event_object in handle_user_message(chat_id, user_input):
+            print(get_output_text(event_object))
