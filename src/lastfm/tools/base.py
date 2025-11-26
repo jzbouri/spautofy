a7d5dc79d4a87e10240f1f3052b286cb7feb04eb
Jalal Bouri <jalal@hiddenharvestventures.com>
2025-11-26 11:33:37 -0500

    add base api request for lastfm

---
+++ src/lastfm/tools/base.py
@@ -0,0 +1,21 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("LASTFM_API_KEY")
+
+base_url = "https://ws.audioscrobbler.com/2.0/"
+
+def base_api_request(route: str, method: str, params: dict = None, body: dict = None):
+    
+    if params is None:
+        params = {}
+        
+    params["api_key"] = api_key
+    params["format"] = "json"
+    
+    response = requests.request(method, f"{base_url}{route}", params=params, data=body)
+    
+    return response.text
