938528ab3031e5eb0ab9d88a7f085f63de47b331
Anmeet Sekhon <Anmeet31@gmail.com>
2026-03-08 03:01:48 -0700

    download python packages

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 sudo cp spautofy /usr/local/bin/spautofy
 sudo chmod +x /usr/local/bin/spautofy
 echo $PWD | tee $HOME/spautofylocation.txt
+python3 -m venv .venv
+pip install -e .
+pip install -r requirements.txt
